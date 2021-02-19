5514ca7266d245f65b5ba45a007c7984c6e21321
icYFTL <savap0@yandex.ru>
2021-02-19 21:08:54 +0300

    mail fixes 2

--- source/mail/mail.py
+++ source/mail/mail.py
@@ -4,4 +4,4 @@ from os import system
 class Mail:
     @staticmethod
     def send_confirmation(to: str, code: str):
-        system(f'echo "Your code is <h1>{code}</h1>" | /opt/bin/mail -s "BootCore code" {to}')
+        system(f'echo "Your code is <h1>{code}</h1>" | /opt/mail/mail -s "BootCore code" {to}')
