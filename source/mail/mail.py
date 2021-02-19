eb454c7d3a1806c7399b92ffb62985c2b68b41e0
icYFTL <savap0@yandex.ru>
2021-02-19 18:43:07 +0300

    Mail & docker fixes 1

--- source/mail/mail.py
+++ source/mail/mail.py
@@ -4,4 +4,4 @@ from os import system
 class Mail:
     @staticmethod
     def send_confirmation(to: str, code: str):
-        system(f'echo "Your code is <h1>{code}</h1>" | mail -s "BootCore code" {to}')
+        system(f'echo "Your code is <h1>{code}</h1>" | /opt/bin/mail -s "BootCore code" {to}')
