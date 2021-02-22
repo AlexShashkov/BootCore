9b516b3ee6ed430041369ca47c8eca06a1e16371
icYFTL <savap0@yandex.ru>
2021-02-23 00:59:23 +0300

    fixes

--- source/mail/mail.py
+++ source/mail/mail.py
@@ -1,7 +1,15 @@
-from os import system
+import requests
+
+from core import mail_conf
 
 
 class Mail:
     @staticmethod
-    def send_confirmation(to: str, code: str):
-        system(f'echo "Your code is <h1>{code}</h1>" | /opt/mail/mail -s "BootCore code" {to}')
+    def send_confirmation(to: str, code: str) -> int:
+        return requests.post(
+            f"https://api.mailgun.net/v3/{mail_conf['api_host']}/messages",
+            auth=("api", mail_conf['api_key']),
+            data={"from": f"Slave <bootcore@bootcore.icyftl.ru>",
+                  "to": f"Somebody <{to}>",
+                  "subject": "Bootcore code",
+                  "text": f"Ok then. Your code is {code}"}).status_code
