5a3dbe236931bdf5979e80eef51d7561da3561b6
icyftl <savap0@yandex.ru>
2021-02-26 01:39:44 +0300

    Postgres ready

--- source/database/__init__.py
+++ source/database/__init__.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from core import db_conf
 
-engine = create_engine('sqlite:///test.db', echo=False)
+engine = create_engine('postgresql+psycopg2://{user}:{password}//:@{host}:{port}/{db}'.format(
+    user=db_conf['user'],
+    password=db_conf['password'],
+    db=db_conf['db'],
+    host=db_conf['host'],
+    port=db_conf['port']
+), echo=False)
 Base = declarative_base()
 from .models import *
 
