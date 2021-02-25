21e236f4684ad98c4702550dcc06a9ac93a8bb1e
icyftl <savap0@yandex.ru>
2021-02-26 01:50:05 +0300

    conf fix

--- core.py
+++ core.py
@@ -1,8 +1,8 @@
 import json
 
-__version__ = '0.1.3'
+__version__ = '0.1.3.1'
 
 config = json.load(open('config.json', 'r'))
 api_conf = config['api']
 mail_conf = config['mail']
-db_conf = config['db']
+db_conf = config['database']
